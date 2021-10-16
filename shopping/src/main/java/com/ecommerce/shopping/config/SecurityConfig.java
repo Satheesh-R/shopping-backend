9fa22e30fe59c76433c7e487a799b7048636804d
Satheesh-R <satheesh.rsm1@gmail.com>
2021-10-16 22:29:16 +0530

    authorization added for orders API

---
+++ shopping/src/main/java/com/ecommerce/shopping/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.ecommerce.shopping.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //protect /api/orders endpoint
+        http.authorizeRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        //add cors filter
+        http.cors();
+
+        //return a response body for 401 status
+        Okta.configureResourceServer401ResponseBody(http);
+    }
+}
