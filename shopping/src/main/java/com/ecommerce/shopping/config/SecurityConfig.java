b7613e6ff5bae0ebf1c59f4c5bff3cadbf7bf2ff
Satheesh-R <satheesh.rsm1@gmail.com>
2021-11-05 22:37:32 +0530

    enabled https

--- shopping/src/main/java/com/ecommerce/shopping/config/SecurityConfig.java
+++ shopping/src/main/java/com/ecommerce/shopping/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.ecommerce.shopping.config;
 import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -21,5 +23,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         //return a response body for 401 status
         Okta.configureResourceServer401ResponseBody(http);
+
+        //disable CSRF as we are not using cookie to track session
+        http.csrf().disable();
     }
 }
